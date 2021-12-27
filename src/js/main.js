4e24a9ddf1c8ec37ba93acb57a12df62a5ce32fb
@MatevosianArutiun <arutiunmatevosian@gmail.com>
2021-12-27 23:55:17 +0300

    add change

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,9 @@
 "use strict";
 
-import { showLoading } from "./scripts/preloader";
 import { scroll } from "./scripts/scroll";
 
 
 document.addEventListener("DOMContentLoaded", () => {
-  showLoading();
   scroll();
 });
 
